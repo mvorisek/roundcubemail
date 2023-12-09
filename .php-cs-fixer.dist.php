39bcb4aeca989bf916a0342c0e576b235289bd57
Michael Voříšek <mvorisek@mvorisek.cz>
2023-12-09 19:57:35 +0100

    fix "phpdoc_scalar"

--- .php-cs-fixer.dist.php
+++ .php-cs-fixer.dist.php
@@ -121,7 +121,6 @@ return (new PhpCsFixer\Config())
         'phpdoc_annotation_without_dot' => false,
         'phpdoc_no_alias_tag' => false,
         'phpdoc_no_package' => false,
-        'phpdoc_scalar' => false,
         'phpdoc_single_line_var_spacing' => false,
         'phpdoc_summary' => false,
         'phpdoc_to_comment' => false,
